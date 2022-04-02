df532b32f369df8a12181425f09dd2ac49209a73
andrei2528 <67557344+andrei2528@users.noreply.github.com>
2022-04-02 11:15:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+        'X-RapidAPI-Key': '09668cd7admshefc234a266c2907p1e4d2ejsne787cb53ab0f'
+    }
+};
+
+let current = 0;
+
+async function pageReload() {
+    try {
+        const showPrice = document.getElementById("bitcoin");
+        let response = await fetch('https://alpha-vantage.p.rapidapi.com/query?from_currency=BTC&function=CURRENCY_EXCHANGE_RATE&to_currency=USD', options);
+        let data = await response.json();
+        console.log(data);
+        let pastValue = parseInt(data["Realtime Currency Exchange Rate"]["8. Bid Price"], 10);
+        showPrice.innerHTML = pastValue;
+        if (current > pastValue) {
+            showPrice.style.color = 'green';
+        } else if (current == pastValue) {
+            showPrice.style.color = 'grey';
+        } else if (current < pastValue) {
+            console.log("current este " + current + ' pastValue este' + pastValue)
+            showPrice.style.color = 'red';
+        }
+        current = pastValue;
+    } catch (e) {
+        console.log(e);
+    }
+}
+pageReload();
+const timer = setInterval(pageReload, 20000);
+setInterval();
